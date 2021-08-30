60f0bab2c0eabbc2483d39a440b1a4227978923d
mr-atta <mohammadatta97@gmail.com>
2021-08-30 17:15:31 +0300

    lab 12

--- caps.js
+++ caps.js
@@ -1,24 +1,57 @@
 "use strict";
 
-const events = require("./events"); // require the events
-require("dotenv").config();
-const faker = require("faker");
+// const events = require("./events"); // require the events
+
+require("dotenv").config(); // dotenv
+
+// socket.io
+const io = require("socket.io")(process.env.PORT || 3000); // http://localhost:3020
+// namespace
+const capsSystem = io.of("/caps"); //  io.of('/') >>> http://localhost:3020/caps
 
 // require stor parts
-require("./store-parts/Drivers/driver");
-require("./store-parts/Vendor/vendor");
+// require("./vendor");
+// require("./driver");
 //
 
-//
-setInterval(() => {
-  // data
-  let data = {
-    store: process.env.STOR_NAME,
-    orderID: faker.datatype.uuid(),
-    customer: faker.name.findName(),
-    address: faker.address.streetAddress(),
-    // DriverName: faker.name.findName(),
-  };
+io.on("connection", (socket) => {
+  console.log("connection to the caps >> done");
+});
 
-  events.emit("pickup", data);
-}, 5000); // 5 seconds
+capsSystem.on("connection", (socket) => {
+  socket.on("pickup", (payload) => {
+    // call
+    outputStorData("pickup", payload);
+    // emit
+    capsSystem.emit("driver", payload);
+  });
+  // from driver.js
+  socket.on("in-transit", (payload) => {
+    // call
+    outputStorData("in-transit", payload);
+  });
+  // from driver.js
+  socket.on("delivered", (payload) => {
+    // call
+    outputStorData("delivered", payload);
+    // emit
+    capsSystem.emit("done", payload);
+  });
+});
+
+function outputStorData(socket, payload) {
+  // time
+  let today = new Date();
+  let date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let dateTime = date + " " + time;
+
+  //
+  console.log(`EVENT { event: ${socket},`);
+  console.log(`time: ${dateTime}`);
+  console.log(`payload:`);
+  console.log(payload);
+  //
+}
